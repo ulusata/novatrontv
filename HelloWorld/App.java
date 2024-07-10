8f880dd4e38952af9648008008c414a4f0b8bb1e
ulusata <ulusataaslan06@gmail.com>
2024-07-10 17:05:38 +0300

    add hello world

---
+++ HelloWorld/App.java
@@ -0,0 +1,10 @@
+//Dosya adı ile class ismi aynı olmalıdır.
+public class App {
+    //bu main fonksiyonu tüm kodlarımızı çalıştırabilmemizi sağlar.
+    public static void main(String[] args){
+
+        System.out.println("Hello World!"); //Ekrana mesajı yazar.
+
+        System.out.println("NovatronTV!");
+    }
+}
